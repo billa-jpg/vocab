a8ab6f6e173dc4ab5d24559efc16c940fdf197e7
billa-jpg <0908335051bill@gmail.com>
2026-09-12 19:42:01 +0800

    Create level4.js

---
+++ level4.js
@@ -0,0 +1,10 @@
+const WORDS_LEVEL_4 = [
+  {
+    word: "efficient",
+    pos: "adj.",
+    defEn: "Working in a well-organized and competent way without wasting time, energy, or resources.",
+    defZh: "有效率的 (Level 4)",
+    ex1: "This new machine is very efficient.",
+    ex2: "Developing efficient numerical algorithms allows scientists to solve complex differential equations within seconds."
+  }
+];
