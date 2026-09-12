8d64af88900d25ff722c9a941dae6f0bf9b9b167
billa-jpg <0908335051bill@gmail.com>
2026-09-12 19:41:07 +0800

    Create level3.js

---
+++ level3.js
@@ -0,0 +1,10 @@
+const WORDS_LEVEL_3 = [
+  {
+    word: "constant",
+    pos: "adj.",
+    defEn: "Happening all the time or repeatedly; remaining unchanging.",
+    defZh: "持續不斷的；恆定的 (Level 3)",
+    ex1: "He was tired of the constant noise.",
+    ex2: "Maintaining a constant velocity requires balancing the applied driving force against opposing frictional drag."
+  }
+];
